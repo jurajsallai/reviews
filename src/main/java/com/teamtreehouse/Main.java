e55e75382e17432690260ff5cff7b6e95776728f
Juraj Sallai <jurajsallai@gmail.com>
2018-07-13 17:46:54 +0200

    Initial commit

---
+++ src/main/java/com/teamtreehouse/Main.java
@@ -0,0 +1,22 @@
+package com.teamtreehouse;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.EXCEL);
+            printer.printRecord("Craig", 5, "Ahoj, ako sa mas");
+            printer.printRecord("Juraj", 4, "Ahoj, ako, sa mas");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+}
