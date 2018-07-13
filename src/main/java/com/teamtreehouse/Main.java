b41970619c6316a7b3eaecc89dc383e295c23e74
Juraj Sallai <jurajsallai@gmail.com>
2018-07-13 18:10:11 +0200

    Add user Martin

--- src/main/java/com/teamtreehouse/Main.java
+++ src/main/java/com/teamtreehouse/Main.java
@@ -11,6 +11,7 @@ public class Main {
             CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.EXCEL);
             printer.printRecord("Craig", 5, "Ahoj, ako sa mas");
             printer.printRecord("Juraj", 4, "Ahoj, ako, sa mas");
+            printer.printRecord("Martin", 5, "blabla");
         } catch (IOException e) {
             e.printStackTrace();
         }
